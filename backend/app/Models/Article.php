c10c043512de531f396eb52b44d42a346e9be2ab
Filipe Macedo <filipe.macedo@doutorfinancas.pt>
2023-01-31 22:05:18 +0000

    initial commit

---
+++ backend/app/Models/Article.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    private $fillable = [
+        'title', 'body', 'author'
+    ];
+
+    private $table = 'articles';
+}
